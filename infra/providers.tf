d6863868a3af7e69444fabcbc7a6902a363a791a
Gbolahan.dev <gbolahan.adekanbi.dev@gmail.com>
2025-07-06 08:23:20 +0100

    edited providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,12 +1,14 @@
+# infra/providers.tf
+
 terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"          #  ← 5.x, NOT 6.x
+      version = "~> 5.0"
     }
-    google-beta = {               #  must match google
+    google-beta = {
       source  = "hashicorp/google-beta"
       version = "~> 5.0"
     }
@@ -29,17 +31,30 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-data "google_client_config" "default" {}
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.main.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.main.master_auth[0].cluster_ca_certificate)
-}
-provider "helm" {
-  kubernetes {
-    host                   = "https://${google_container_cluster.main.endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(google_container_cluster.main.master_auth[0].cluster_ca_certificate)
-  }
+
+# --- NEW/UPDATED SECTION ---
+
+# 1. Add a data source to look up the GKE cluster
+data "google_container_cluster" "main" {
+  name     = var.cluster_name
+  location = "us-central1-a"
+  project  = var.project_id
 }
 
+# 2. Add a data source to get the authentication token
+data "google_client_config" "default" {}
+
+# 3. Configure the providers to use the data from the lookups
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.main.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.main.master_auth[0].cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.main.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.main.master_auth[0].cluster_ca_certificate)
+  }
+}
